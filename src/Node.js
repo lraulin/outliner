ead484f515bd28ae1cf9c35daf51ae3d71e57dea
RCB User <rcbuser@RCBs-MacBook-Pro.local>
2024-06-05 09:31:42 -0400

    add buttons

--- src/Node.js
+++ src/Node.js
@@ -3,6 +3,15 @@ import { TreeNode } from "./tree";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlusSquare,
+  faArrowAltCircleUp,
+  faArrowAltCircleDown,
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { ButtonGroup, ButtonToolbar } from "react-bootstrap";
 
 /**
  * Description placeholder
@@ -11,7 +20,7 @@ import Button from "react-bootstrap/Button";
  * @param {TreeNode} param0.node
  * @returns {*}
  */
-const Node = ({ node, handleShow }) => {
+const Node = ({ node, handleShow, handleDelete }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Header>{node?.value}</Card.Header>
@@ -24,9 +33,30 @@ const Node = ({ node, handleShow }) => {
           ))}
       </ListGroup>
       <Card.Footer>
-        <Button variant="primary" onClick={() => handleShow(node.key)}>
-          +
-        </Button>
+        <ButtonToolbar aria-label="Toolbar with button groups">
+          <ButtonGroup size="sm" aria-label="Buttons to move item">
+            <Button variant="secondary">
+              <FontAwesomeIcon icon={faArrowAltCircleUp} />
+            </Button>
+            <Button variant="secondary">
+              <FontAwesomeIcon icon={faArrowAltCircleDown} />
+            </Button>
+            <Button variant="secondary">
+              <FontAwesomeIcon icon={faArrowAltCircleLeft} />
+            </Button>
+            <Button variant="secondary">
+              <FontAwesomeIcon icon={faArrowAltCircleRight} />
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup size="sm" aria-label="Buttons to add or delete item">
+            <Button variant="danger" onClick={() => handleDelete(node.key)}>
+              <FontAwesomeIcon icon={faPlusSquare} />
+            </Button>
+            <Button variant="primary" onClick={() => handleShow(node.key)}>
+              <FontAwesomeIcon icon={faPlusSquare} />
+            </Button>
+          </ButtonGroup>
+        </ButtonToolbar>
       </Card.Footer>
     </Card>
   );
